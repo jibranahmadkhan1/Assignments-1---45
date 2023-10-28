cd18d475269a9f76fcac4dca1123906dad42b9c8
jibranahmadkhan1 <141355419+jibranahmadkhan1@users.noreply.github.com>
2023-10-28 11:52:47 +0500

    Add files via upload

---
+++ Exercise 2/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//	Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today
+// Storing the person's name 
+let personName = "Jibran";
+// Creating message
+let message = `Hello ${personName}, would you like to learn some TypeScript today?`;
+console.log(message);
