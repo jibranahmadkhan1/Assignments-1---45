82f1d18099001c5e0e2aa3bb3f0c704ffd4401d1
jibranahmadkhan1 <141355419+jibranahmadkhan1@users.noreply.github.com>
2023-10-28 11:57:38 +0500

    Add files via upload

---
+++ Exercise 5/index.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message. Print your message.*/
+//Founder of the Nation Quaid-e-Azam Muhammad Ali Jinnah said 
+//Unity, Faith and Discipline
+let famous_person = "Quaid e Azam Muhammad Ali Jinnah";
+let message = famous_person + ' once said"Unity, Faith and Discipline."';
+console.log(message);
---
+++ Exercise 5/index.ts
@@ -0,0 +1,11 @@
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message. Print your message.*/
+
+//Founder of the Nation Quaid-e-Azam Muhammad Ali Jinnah said 
+//Unity, Faith and Discipline
+
+let famous_person: string = "Quaid e Azam Muhammad Ali Jinnah";
+
+let message: string = famous_person + ' once said"Unity, Faith and Discipline."'
+console.log(message);
+
